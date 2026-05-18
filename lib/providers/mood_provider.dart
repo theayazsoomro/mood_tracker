ce936d5b3cbbdf29ab879c37537eca256271a715
Ayaz Soomro <ayazkhansoomro07@gmail.com>
2026-05-18 12:21:22 +0500

    Remove mock mood entries

--- lib/providers/mood_provider.dart
+++ lib/providers/mood_provider.dart
@@ -6,32 +6,7 @@ class MoodProvider with ChangeNotifier {
   static const int _maxEntries = 7;
 
   // Mock data for mood entries
-  final List<MoodEntry> _entries = [
-    MoodEntry(
-      type: MoodType.happy,
-      date: DateTime.now().subtract(const Duration(days: 6)),
-    ),
-    MoodEntry(
-      type: MoodType.neutral,
-      date: DateTime.now().subtract(const Duration(days: 5)),
-    ),
-    MoodEntry(
-      type: MoodType.sad,
-      date: DateTime.now().subtract(const Duration(days: 4)),
-    ),
-    MoodEntry(
-      type: MoodType.angry,
-      date: DateTime.now().subtract(const Duration(days: 3)),
-    ),
-    MoodEntry(
-      type: MoodType.frustrated,
-      date: DateTime.now().subtract(const Duration(days: 2)),
-    ),
-    MoodEntry(
-      type: MoodType.happy,
-      date: DateTime.now().subtract(const Duration(days: 1)),
-    ),
-  ];
+  final List<MoodEntry> _entries = [];
 
   /// Returns an unmodifiable list of mood entries.
   UnmodifiableListView<MoodEntry> get entries => UnmodifiableListView(_entries);
